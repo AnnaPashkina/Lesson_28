3184a09166a2c8def353800bf49787ad824d00bf
AnnaPashkina <anya230489@gmail.com>
2022-12-13 22:51:46 +0200

    For Lesson

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,78 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        //System.out.println("Hello Anna!");
+    /*int my_number = 5;
+    double second_number;
+    System.out.println("My_number is" + my_number);
+    second_number = 22.5;
+        System.out.println("My_number is" + second_number);
+        String my_string = "My First String";
+        System.out.println(my_string);
+        System.out.println(my_string.toLowerCase());
+        System.out.println(my_string.toUpperCase());
+        System.out.println(my_string.length());
+        System.out.println(my_string.charAt(0));*/
+        //{int a = 10;
+        //int b = 3;
+       /* System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);*/
+        /* {System.out.println(++a);
+        System.out.println(--b);
+        System.out.println(a++);
+        System.out.println(b--);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(" мой текст " + a + " my second text ");*/
+        /*
+        >
+        <
+        >=
+        <=
+        ==
+        !=
+         */
+        /* {System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);*/
+         int age = 17;
+         if (age >= 18) {System.out.println("You are an old person"); }
+         else {System.out.println("You are a child");}
+         int weekDay = 10;
+         (weekDay==1);
+        {System.out.println("Monday");}
+        switch (weekDay) {case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+            System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+                case 6:
+                System.out.println("Saturday");
+                break;
+                case 7:
+                System.out.println("Sunday");
+                break;
+                default:
+                System.out.println("No such day");
+        }
+
+
+
+
+                   }
 }
