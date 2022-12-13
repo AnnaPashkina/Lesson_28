2f234053e0d610a7ec197e548e45bddc05c1ff12
AnnaPashkina <anya230489@gmail.com>
2022-12-13 23:43:34 +0200

    For Lesson

--- src/Homework_1.java
+++ src/Homework_1.java
@@ -3,13 +3,9 @@ public class Homework_1 {
             /*{int currentYear = 2022;
             int friendYear = 1991;
             System.out.println(currentYear-friendYear);*/
-            double number = 135789;
-            double lastDigit = 9;
-
-
-
-
-
+            int number = 135789;
+            int lastDigit = 9;
+            System.out.println(" Last Digit is " + lastDigit);
 
         }
 
