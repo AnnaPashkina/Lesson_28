23f76b3071217cedc929407b6603fbee72972064
AnnaPashkina <anya230489@gmail.com>
2022-12-14 22:05:56 +0200

    For Lesson

--- src/Homework_1.java
+++ src/Homework_1.java
@@ -1,17 +1,33 @@
 public class Homework_1 {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
             /*{int currentYear = 2022;
             int friendYear = 1991;
             System.out.println(currentYear-friendYear);*/
-            int number = 135789;
-            int lastDigit = 9;
-            System.out.println(" Last Digit is " + lastDigit);
-
+        /*int number = 135789;
+        int lastDigit = 9;
+        System.out.println(" Last Digit is " + lastDigit);*/
+        /*int age = 27;
+        if (age >= 18 & age <=30) {
+            System.out.println("Анна, поздравляю ви разобрались как работает if");}*/
+        /*int x = 17;
+        int y = -10;
+        if (x > 0 & y > 0) {System.out.println("Цель находится в первой четверти");}
+        if (x < 0 & y > 0) {System.out.println("Цель находится во второй четверти");}
+        if (x<0 & y<0) {System.out.println("Цель находится в третей четверти");}
+        if (x>0 & y<0) {System.out.println("Цель находится в четвертой четверти");}*/
+        int a=1;
+        int b=2;
+        int c=10;
+        if ((a+b)>c & (b+c)>a & (a+c)>b) {System.out.println("Такой треугольник можно построить");}
+        else System.out.println("Такой треугольник нельзя построить");
         }
+    }
 
 
 
-        }
+
+
+
 
 
 
